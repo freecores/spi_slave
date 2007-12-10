14496faa08760e3a55a75b0ffdeef114ac5ddc69
dkoethe <dkoethe@607bfbf4-301f-4581-93e3-6ae442bf32ca>
2007-12-10 20:48:33 +0000

    Version 1.1 Bugfix added syncronisation registers opb_fifo_flg_int_r[0,1] to prevent metastability

--- pcore/opb_spi_slave_v1_00_a/hdl/vhdl/irq_ctl.vhd
+++ pcore/opb_spi_slave_v1_00_a/hdl/vhdl/irq_ctl.vhd
@@ -8,6 +8,10 @@
 --*   @version: 1.0
 --* @date:      2007-11-11
 --/
+-- Version 1.1
+-- Bugfix
+-- added syncronisation registers opb_fifo_flg_int_r[0,1] to prevent
+-- metastability
 -------------------------------------------------------------------------------
 library ieee;
 use ieee.std_logic_1164.all;
@@ -28,19 +32,28 @@ end irq_ctl;
 architecture behavior of irq_ctl is
 
   signal opb_fifo_flg_int : std_logic;
+  -- Sync to clock domain register
+  signal opb_fifo_flg_int_r0 : std_logic;
+  signal opb_fifo_flg_int_r1 : std_logic;
+
+
   signal opb_fifo_flg_reg : std_logic;
 begin  -- behavior
 
-  opb_fifo_flg_int <= opb_fifo_flg when (C_ACTIVE_EDGE = '1') else
-                      not opb_fifo_flg;
+  opb_fifo_flg_int_r0 <= opb_fifo_flg when (C_ACTIVE_EDGE = '1') else
+                         not opb_fifo_flg;
   
-  irq_ctl_proc: process(rst, clk)
+  irq_ctl_proc : process(rst, clk)
   begin
     if (rst = '1') then
       opb_isr <= '0';
     elsif rising_edge(clk) then
+      -- sync to clock domain
+      opb_fifo_flg_int_r1 <= opb_fifo_flg_int_r0;
+      opb_fifo_flg_int    <= opb_fifo_flg_int_r1;
+
       opb_fifo_flg_reg <= opb_fifo_flg_int;
-      if (opb_ier= '1' and opb_fifo_flg_int = '1' and opb_fifo_flg_reg = '0') then
+      if (opb_ier = '1' and opb_fifo_flg_int = '1' and opb_fifo_flg_reg = '0') then
         opb_isr <= '1';
       elsif (opb_isr_clr = '1') then
         opb_isr <= '0';
