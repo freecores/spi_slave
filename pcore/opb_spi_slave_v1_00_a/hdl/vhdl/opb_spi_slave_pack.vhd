769d2c14753076db4d925c2a9f7c4b68e4b367b0
dkoethe <dkoethe@607bfbf4-301f-4581-93e3-6ae442bf32ca>
2008-03-24 19:34:53 +0000

    added constant C_ADR_RX_CRC,C_ADR_TX_CRC, added constant C_OPB_CTL_REG_CRC_EN, C_OPB_CTL_REG_CRC_CLR

--- pcore/opb_spi_slave_v1_00_a/hdl/vhdl/opb_spi_slave_pack.vhd
+++ pcore/opb_spi_slave_v1_00_a/hdl/vhdl/opb_spi_slave_pack.vhd
@@ -18,26 +18,29 @@ package opb_spi_slave_pack is
   constant C_ADR_RX_DMA_CTL  : std_logic_vector(7 downto 2) := conv_std_logic_vector(16#9#, 6);
   constant C_ADR_RX_DMA_ADDR : std_logic_vector(7 downto 2) := conv_std_logic_vector(16#A#, 6);
   constant C_ADR_RX_DMA_NUM  : std_logic_vector(7 downto 2) := conv_std_logic_vector(16#B#, 6);
+  constant C_ADR_RX_CRC      : std_logic_vector(7 downto 2) := conv_std_logic_vector(16#C#, 6);
+  constant C_ADR_TX_CRC      : std_logic_vector(7 downto 2) := conv_std_logic_vector(16#D#, 6);
 
 -- XIIF_V123B compatible
   constant C_ADR_DGIE : std_logic_vector(7 downto 2) := conv_std_logic_vector(16#10#, 6);
   constant C_ADR_ISR  : std_logic_vector(7 downto 2) := conv_std_logic_vector(16#11#, 6);
   constant C_ADR_IER  : std_logic_vector(7 downto 2) := conv_std_logic_vector(16#12#, 6);
 
-  constant C_NUM_FLG           : integer := 16;
-  constant C_NUM_INT           : integer := 11;
+  constant C_NUM_FLG       : integer := 16;
+  constant C_NUM_INT       : integer := 11;
   constant C_WIDTH_DMA_NUM : integer := 24;
 
 
 -- CTL_Register
   -- width
-  constant C_OPB_CTL_REG_WIDTH : integer := 4;
+  constant C_OPB_CTL_REG_WIDTH   : integer := 6;
   -- bits
-  constant C_OPB_CTL_REG_DGE   : integer := 0;
-  constant C_OPB_CTL_REG_TX_EN : integer := 1;  
-  constant C_OPB_CTL_REG_RX_EN : integer := 2;
-  constant C_OPB_CTL_REG_RST   : integer := 3;
-
+  constant C_OPB_CTL_REG_DGE     : integer := 0;
+  constant C_OPB_CTL_REG_TX_EN   : integer := 1;
+  constant C_OPB_CTL_REG_RX_EN   : integer := 2;
+  constant C_OPB_CTL_REG_RST     : integer := 3;
+  constant C_OPB_CTL_REG_CRC_EN  : integer := 4;
+  constant C_OPB_CTL_REG_CRC_CLR : integer := 5;
 
   -- Status Register
   constant SPI_SR_Bit_TX_Prog_Full  : integer := 0;
@@ -55,7 +58,9 @@ package opb_spi_slave_pack is
   constant SPI_SR_Bit_RX_Underflow  : integer := 11;
 
   constant SPI_SR_Bit_SS_n : integer := 12;
-
+  constant SPI_SR_Bit_TX_DMA_Done : integer := 13;
+  constant SPI_SR_Bit_RX_DMA_Done : integer := 14;
+  
   -- Interrupt Status Register
   constant SPI_ISR_Bit_TX_Prog_Empty : integer := 0;
   constant SPI_ISR_Bit_TX_Empty      : integer := 1;
@@ -64,5 +69,5 @@ package opb_spi_slave_pack is
   constant SPI_ISR_Bit_RX_Full       : integer := 4;
   constant SPI_ISR_Bit_RX_Overflow   : integer := 5;
   constant SPI_ISR_Bit_SS_Fall       : integer := 6;
-  constant SPI_ISR_Bit_SS_Rise       : integer := 7;  
+  constant SPI_ISR_Bit_SS_Rise       : integer := 7;
 end opb_spi_slave_pack;
