d1c84055c97827c26b070fba7b2759ad211bb6af
dkoethe <dkoethe@607bfbf4-301f-4581-93e3-6ae442bf32ca>
2008-03-24 19:36:24 +0000

    added logic for CRC-Generation

--- pcore/opb_spi_slave_v1_00_a/hdl/vhdl/opb_spi_slave.vhd
+++ pcore/opb_spi_slave_v1_00_a/hdl/vhdl/opb_spi_slave.vhd
@@ -42,7 +42,8 @@ entity opb_spi_slave is
     C_CPOL            : integer range 0 to 1 := 0;
     C_PHA             : integer range 0 to 1 := 0;
     C_FIFO_SIZE_WIDTH : integer range 4 to 7 := 5;  -- depth 32
-    C_DMA_EN          : boolean              := false);
+    C_DMA_EN          : boolean              := false;
+    C_CRC_EN          : boolean              := false);
 
   port (
     -- OPB signals (Slave Side)
@@ -100,39 +101,43 @@ architecture behavior of opb_spi_slave is
       C_FAMILY          : string;
       C_SR_WIDTH        : integer;
       C_FIFO_SIZE_WIDTH : integer;
-      C_DMA_EN          : boolean);
+      C_DMA_EN          : boolean;
+      C_CRC_EN          : boolean);
     port (
-      OPB_ABus        : in  std_logic_vector(0 to C_OPB_AWIDTH-1);
-      OPB_BE          : in  std_logic_vector(0 to C_OPB_DWIDTH/8-1);
-      OPB_Clk         : in  std_logic;
-      OPB_DBus        : in  std_logic_vector(0 to C_OPB_DWIDTH-1);
-      OPB_RNW         : in  std_logic;
-      OPB_Rst         : in  std_logic;
-      OPB_select      : in  std_logic;
-      OPB_seqAddr     : in  std_logic;
-      Sln_DBus        : out std_logic_vector(0 to C_OPB_DWIDTH-1);
-      Sln_errAck      : out std_logic;
-      Sln_retry       : out std_logic;
-      Sln_toutSup     : out std_logic;
-      Sln_xferAck     : out std_logic;
-      opb_s_tx_en     : out std_logic;
-      opb_s_tx_data   : out std_logic_vector(C_SR_WIDTH-1 downto 0);
-      opb_s_rx_en     : out std_logic;
-      opb_s_rx_data   : in  std_logic_vector(C_SR_WIDTH-1 downto 0);
-      opb_ctl_reg     : out std_logic_vector(C_OPB_CTL_REG_WIDTH-1 downto 0);
-      tx_thresh       : out std_logic_vector((2*C_FIFO_SIZE_WIDTH)-1 downto 0);
-      rx_thresh       : out std_logic_vector((2*C_FIFO_SIZE_WIDTH)-1 downto 0);
-      opb_fifo_flg    : in  std_logic_vector(C_NUM_FLG-1 downto 0);
-      opb_dgie        : out std_logic;
-      opb_ier         : out std_logic_vector(C_NUM_INT-1 downto 0);
-      opb_isr         : in  std_logic_vector(C_NUM_INT-1 downto 0);
-      opb_isr_clr     : out std_logic_vector(C_NUM_INT-1 downto 0);
-      opb_tx_dma_addr : out std_logic_vector(C_OPB_DWIDTH-1 downto 0);
-      opb_tx_dma_ctl  : out std_logic_vector(0 downto 0);
-      opb_tx_dma_num  : out std_logic_vector(C_WIDTH_DMA_NUM-1 downto 0);
-      opb_rx_dma_addr : out std_logic_vector(C_OPB_DWIDTH-1 downto 0);
-      opb_rx_dma_ctl  : out std_logic_vector(0 downto 0);
-      opb_rx_dma_num  : out std_logic_vector(C_WIDTH_DMA_NUM-1 downto 0));
+      OPB_ABus         : in  std_logic_vector(0 to C_OPB_AWIDTH-1);
+      OPB_BE           : in  std_logic_vector(0 to C_OPB_DWIDTH/8-1);
+      OPB_Clk          : in  std_logic;
+      OPB_DBus         : in  std_logic_vector(0 to C_OPB_DWIDTH-1);
+      OPB_RNW          : in  std_logic;
+      OPB_Rst          : in  std_logic;
+      OPB_select       : in  std_logic;
+      OPB_seqAddr      : in  std_logic;
+      Sln_DBus         : out std_logic_vector(0 to C_OPB_DWIDTH-1);
+      Sln_errAck       : out std_logic;
+      Sln_retry        : out std_logic;
+      Sln_toutSup      : out std_logic;
+      Sln_xferAck      : out std_logic;
+      opb_s_tx_en      : out std_logic;
+      opb_s_tx_data    : out std_logic_vector(C_SR_WIDTH-1 downto 0);
+      opb_s_rx_en      : out std_logic;
+      opb_s_rx_data    : in  std_logic_vector(C_SR_WIDTH-1 downto 0);
+      opb_ctl_reg      : out std_logic_vector(C_OPB_CTL_REG_WIDTH-1 downto 0);
+      tx_thresh        : out std_logic_vector((2*C_FIFO_SIZE_WIDTH)-1 downto 0);
+      rx_thresh        : out std_logic_vector((2*C_FIFO_SIZE_WIDTH)-1 downto 0);
+      opb_fifo_flg     : in  std_logic_vector(C_NUM_FLG-1 downto 0);
+      opb_dgie         : out std_logic;
+      opb_ier          : out std_logic_vector(C_NUM_INT-1 downto 0);
+      opb_isr          : in  std_logic_vector(C_NUM_INT-1 downto 0);
+      opb_isr_clr      : out std_logic_vector(C_NUM_INT-1 downto 0);
+      opb_tx_dma_addr  : out std_logic_vector(C_OPB_DWIDTH-1 downto 0);
+      opb_tx_dma_ctl   : out std_logic_vector(0 downto 0);
+      opb_tx_dma_num   : out std_logic_vector(C_WIDTH_DMA_NUM-1 downto 0);
+      opb_rx_dma_addr  : out std_logic_vector(C_OPB_DWIDTH-1 downto 0);
+      opb_rx_dma_ctl   : out std_logic_vector(0 downto 0);
+      opb_rx_dma_num   : out std_logic_vector(C_WIDTH_DMA_NUM-1 downto 0);
+      opb_rx_crc_value : in  std_logic_vector(C_SR_WIDTH-1 downto 0);
+      opb_tx_crc_value : in  std_logic_vector(C_SR_WIDTH-1 downto 0));
+
   end component;
 
 
@@ -150,37 +155,38 @@ architecture behavior of opb_spi_slave is
       C_PHA             : integer range 0 to 1;
       C_FIFO_SIZE_WIDTH : integer range 4 to 7);
     port (
-      OPB_Clk         : in  std_logic;
-      OPB_Rst         : in  std_logic;
-      OPB_DBus        : in  std_logic_vector(0 to C_OPB_DWIDTH-1);
-      M_request       : out std_logic;
-      MOPB_MGrant     : in  std_logic;
-      M_busLock       : out std_logic;
-      M_ABus          : out std_logic_vector(0 to C_OPB_AWIDTH-1);
-      M_BE            : out std_logic_vector(0 to C_OPB_DWIDTH/8-1);
-      M_DBus          : out std_logic_vector(0 to C_OPB_DWIDTH-1);
-      M_RNW           : out std_logic;
-      M_select        : out std_logic;
-      M_seqAddr       : out std_logic;
-      MOPB_errAck     : in  std_logic;
-      MOPB_retry      : in  std_logic;
-      MOPB_timeout    : in  std_logic;
-      MOPB_xferAck    : in  std_logic;
-      opb_m_tx_req    : in  std_logic;
-      opb_m_tx_en     : out std_logic;
-      opb_m_tx_data   : out std_logic_vector(C_SR_WIDTH-1 downto 0);
-      opb_tx_dma_ctl  : in  std_logic_vector(0 downto 0);
-      opb_tx_dma_addr : in  std_logic_vector(C_OPB_DWIDTH-1 downto 0);
-      opb_tx_dma_num  : in  std_logic_vector(C_WIDTH_DMA_NUM-1 downto 0);
-      opb_tx_dma_done : out std_logic;
-      opb_m_rx_req    : in  std_logic;
-      opb_m_rx_en     : out std_logic;
-      opb_m_rx_data   : in  std_logic_vector(C_SR_WIDTH-1 downto 0);
-      opb_rx_dma_ctl  : in  std_logic_vector(0 downto 0);
-      opb_rx_dma_addr : in  std_logic_vector(C_OPB_DWIDTH-1 downto 0);
-      opb_rx_dma_num  : in  std_logic_vector(C_WIDTH_DMA_NUM-1 downto 0);
-      opb_rx_dma_done : out std_logic;
-      opb_abort_flg   : out std_logic);
+      OPB_Clk          : in  std_logic;
+      OPB_Rst          : in  std_logic;
+      OPB_DBus         : in  std_logic_vector(0 to C_OPB_DWIDTH-1);
+      M_request        : out std_logic;
+      MOPB_MGrant      : in  std_logic;
+      M_busLock        : out std_logic;
+      M_ABus           : out std_logic_vector(0 to C_OPB_AWIDTH-1);
+      M_BE             : out std_logic_vector(0 to C_OPB_DWIDTH/8-1);
+      M_DBus           : out std_logic_vector(0 to C_OPB_DWIDTH-1);
+      M_RNW            : out std_logic;
+      M_select         : out std_logic;
+      M_seqAddr        : out std_logic;
+      MOPB_errAck      : in  std_logic;
+      MOPB_retry       : in  std_logic;
+      MOPB_timeout     : in  std_logic;
+      MOPB_xferAck     : in  std_logic;
+      opb_m_tx_req     : in  std_logic;
+      opb_m_tx_en      : out std_logic;
+      opb_m_tx_data    : out std_logic_vector(C_SR_WIDTH-1 downto 0);
+      opb_tx_dma_ctl   : in  std_logic_vector(0 downto 0);
+      opb_tx_dma_addr  : in  std_logic_vector(C_OPB_DWIDTH-1 downto 0);
+      opb_tx_dma_num   : in  std_logic_vector(C_WIDTH_DMA_NUM-1 downto 0);
+      opb_tx_dma_done  : out std_logic;
+      opb_m_rx_req     : in  std_logic;
+      opb_m_rx_en      : out std_logic;
+      opb_m_rx_data    : in  std_logic_vector(C_SR_WIDTH-1 downto 0);
+      opb_rx_dma_ctl   : in  std_logic_vector(0 downto 0);
+      opb_rx_dma_addr  : in  std_logic_vector(C_OPB_DWIDTH-1 downto 0);
+      opb_rx_dma_num   : in  std_logic_vector(C_WIDTH_DMA_NUM-1 downto 0);
+      opb_rx_dma_done  : out std_logic;
+      opb_abort_flg    : out std_logic;
+      opb_m_last_block : out std_logic);
   end component;
 
   component shift_register
@@ -241,6 +247,24 @@ architecture behavior of opb_spi_slave is
       opb_isr_clr  : in  std_logic);
   end component;
 
+  component crc_core
+    generic (
+      C_SR_WIDTH : integer);
+    port (
+      rst              : in  std_logic;
+      opb_clk          : in  std_logic;
+      crc_en           : in  std_logic;
+      crc_clr          : in  std_logic;
+      opb_m_last_block : in  std_logic;
+      fifo_rx_en       : in  std_logic;
+      fifo_rx_data     : in  std_logic_vector(C_SR_WIDTH-1 downto 0);
+      opb_rx_crc_value : out std_logic_vector(C_SR_WIDTH-1 downto 0);
+      fifo_tx_en       : in  std_logic;
+      fifo_tx_data     : in  std_logic_vector(C_SR_WIDTH-1 downto 0);
+      tx_crc_insert    : out std_logic;
+      opb_tx_crc_value : out std_logic_vector(C_SR_WIDTH-1 downto 0));
+  end component;
+
 -- opb_if
   signal opb_ctl_reg : std_logic_vector(C_OPB_CTL_REG_WIDTH-1 downto 0);
 
@@ -259,12 +283,16 @@ architecture behavior of opb_spi_slave is
   signal opb_rx_dma_ctl  : std_logic_vector(0 downto 0);
   signal opb_rx_dma_num  : std_logic_vector(C_WIDTH_DMA_NUM-1 downto 0);
 
+  signal opb_rx_crc_value : std_logic_vector(C_SR_WIDTH-1 downto 0);
+  signal opb_tx_crc_value : std_logic_vector(C_SR_WIDTH-1 downto 0);
+
   -- opb_m_if
-  signal opb_m_tx_en   : std_logic;
-  signal opb_m_tx_data : std_logic_vector(C_SR_WIDTH-1 downto 0);
-  signal opb_m_rx_en   : std_logic;
-  signal opb_m_rx_data : std_logic_vector(C_SR_WIDTH-1 downto 0);
-  signal opb_abort_flg : std_logic;
+  signal opb_m_tx_en      : std_logic;
+  signal opb_m_tx_data    : std_logic_vector(C_SR_WIDTH-1 downto 0);
+  signal opb_m_rx_en      : std_logic;
+  signal opb_m_rx_data    : std_logic_vector(C_SR_WIDTH-1 downto 0);
+  signal opb_abort_flg    : std_logic;
+  signal opb_m_last_block : std_logic;
 
 -- shift_register
   signal sr_tx_clk  : std_logic;
@@ -292,7 +320,12 @@ architecture behavior of opb_spi_slave is
   signal fifo_tx_data : std_logic_vector(C_SR_WIDTH-1 downto 0);
   signal fifo_rx_en   : std_logic;
   signal fifo_rx_data : std_logic_vector(C_SR_WIDTH-1 downto 0);
-  
+
+  -- rx crc_core
+  signal crc_clr       : std_logic;
+  signal crc_en        : std_logic;
+  signal tx_crc_insert : std_logic;
+
 begin  -- behavior
 
   --* 
@@ -330,39 +363,42 @@ begin  -- behavior
       C_FAMILY          => C_FAMILY,
       C_SR_WIDTH        => C_SR_WIDTH,
       C_FIFO_SIZE_WIDTH => C_FIFO_SIZE_WIDTH,
-      C_DMA_EN          => C_DMA_EN)
+      C_DMA_EN          => C_DMA_EN,
+      C_CRC_EN          => C_CRC_EN)
     port map (
-      OPB_ABus        => OPB_ABus,
-      OPB_BE          => OPB_BE,
-      OPB_Clk         => OPB_Clk,
-      OPB_DBus        => OPB_DBus,
-      OPB_RNW         => OPB_RNW,
-      OPB_Rst         => OPB_Rst,
-      OPB_select      => OPB_select,
-      OPB_seqAddr     => OPB_seqAddr,
-      Sln_DBus        => Sln_DBus,
-      Sln_errAck      => Sln_errAck,
-      Sln_retry       => Sln_retry,
-      Sln_toutSup     => Sln_toutSup,
-      Sln_xferAck     => Sln_xferAck,
-      opb_s_tx_en     => opb_s_tx_en,
-      opb_s_tx_data   => opb_s_tx_data,
-      opb_s_rx_en     => opb_s_rx_en,
-      opb_s_rx_data   => opb_s_rx_data,
-      opb_ctl_reg     => opb_ctl_reg,
-      tx_thresh       => tx_thresh,
-      rx_thresh       => rx_thresh,
-      opb_fifo_flg    => opb_fifo_flg,
-      opb_dgie        => opb_dgie,
-      opb_ier         => opb_ier,
-      opb_isr         => opb_isr,
-      opb_isr_clr     => opb_isr_clr,
-      opb_tx_dma_addr => opb_tx_dma_addr,
-      opb_tx_dma_ctl  => opb_tx_dma_ctl,
-      opb_tx_dma_num  => opb_tx_dma_num,
-      opb_rx_dma_addr => opb_rx_dma_addr,
-      opb_rx_dma_ctl  => opb_rx_dma_ctl,
-      opb_rx_dma_num  => opb_rx_dma_num);
+      OPB_ABus         => OPB_ABus,
+      OPB_BE           => OPB_BE,
+      OPB_Clk          => OPB_Clk,
+      OPB_DBus         => OPB_DBus,
+      OPB_RNW          => OPB_RNW,
+      OPB_Rst          => OPB_Rst,
+      OPB_select       => OPB_select,
+      OPB_seqAddr      => OPB_seqAddr,
+      Sln_DBus         => Sln_DBus,
+      Sln_errAck       => Sln_errAck,
+      Sln_retry        => Sln_retry,
+      Sln_toutSup      => Sln_toutSup,
+      Sln_xferAck      => Sln_xferAck,
+      opb_s_tx_en      => opb_s_tx_en,
+      opb_s_tx_data    => opb_s_tx_data,
+      opb_s_rx_en      => opb_s_rx_en,
+      opb_s_rx_data    => opb_s_rx_data,
+      opb_ctl_reg      => opb_ctl_reg,
+      tx_thresh        => tx_thresh,
+      rx_thresh        => rx_thresh,
+      opb_fifo_flg     => opb_fifo_flg,
+      opb_dgie         => opb_dgie,
+      opb_ier          => opb_ier,
+      opb_isr          => opb_isr,
+      opb_isr_clr      => opb_isr_clr,
+      opb_tx_dma_addr  => opb_tx_dma_addr,
+      opb_tx_dma_ctl   => opb_tx_dma_ctl,
+      opb_tx_dma_num   => opb_tx_dma_num,
+      opb_rx_dma_addr  => opb_rx_dma_addr,
+      opb_rx_dma_ctl   => opb_rx_dma_ctl,
+      opb_rx_dma_num   => opb_rx_dma_num,
+      opb_rx_crc_value => opb_rx_crc_value,
+      opb_tx_crc_value => opb_tx_crc_value);
 
   --* OPB-Master-Interface
   --*
@@ -383,37 +419,38 @@ begin  -- behavior
         C_PHA             => C_PHA,
         C_FIFO_SIZE_WIDTH => C_FIFO_SIZE_WIDTH)
       port map (
-        OPB_Clk         => OPB_Clk,
-        OPB_Rst         => OPB_Rst,
-        OPB_DBus        => OPB_DBus,
-        M_request       => M_request,
-        MOPB_MGrant     => MOPB_MGrant,
-        M_busLock       => M_busLock,
-        M_ABus          => M_ABus,
-        M_BE            => M_BE,
-        M_DBus          => M_DBus,
-        M_RNW           => M_RNW,
-        M_select        => M_select,
-        M_seqAddr       => M_seqAddr,
-        MOPB_errAck     => MOPB_errAck,
-        MOPB_retry      => MOPB_retry,
-        MOPB_timeout    => MOPB_timeout,
-        MOPB_xferAck    => MOPB_xferAck,
-        opb_m_tx_req    => opb_fifo_flg(3),
-        opb_m_tx_en     => opb_m_tx_en,
-        opb_m_tx_data   => opb_m_tx_data,
-        opb_tx_dma_ctl  => opb_tx_dma_ctl,
-        opb_tx_dma_addr => opb_tx_dma_addr,
-        opb_tx_dma_num  => opb_tx_dma_num,
-        opb_tx_dma_done => opb_fifo_flg(13),
-        opb_m_rx_req    => opb_fifo_flg(6),
-        opb_m_rx_en     => opb_m_rx_en,
-        opb_m_rx_data   => opb_m_rx_data,
-        opb_rx_dma_ctl  => opb_rx_dma_ctl,
-        opb_rx_dma_addr => opb_rx_dma_addr,
-        opb_rx_dma_num  => opb_rx_dma_num,
-        opb_rx_dma_done => opb_fifo_flg(14),
-        opb_abort_flg   => opb_abort_flg);
+        OPB_Clk          => OPB_Clk,
+        OPB_Rst          => OPB_Rst,
+        OPB_DBus         => OPB_DBus,
+        M_request        => M_request,
+        MOPB_MGrant      => MOPB_MGrant,
+        M_busLock        => M_busLock,
+        M_ABus           => M_ABus,
+        M_BE             => M_BE,
+        M_DBus           => M_DBus,
+        M_RNW            => M_RNW,
+        M_select         => M_select,
+        M_seqAddr        => M_seqAddr,
+        MOPB_errAck      => MOPB_errAck,
+        MOPB_retry       => MOPB_retry,
+        MOPB_timeout     => MOPB_timeout,
+        MOPB_xferAck     => MOPB_xferAck,
+        opb_m_tx_req     => opb_fifo_flg(3),
+        opb_m_tx_en      => opb_m_tx_en,
+        opb_m_tx_data    => opb_m_tx_data,
+        opb_tx_dma_ctl   => opb_tx_dma_ctl,
+        opb_tx_dma_addr  => opb_tx_dma_addr,
+        opb_tx_dma_num   => opb_tx_dma_num,
+        opb_tx_dma_done  => opb_fifo_flg(13),
+        opb_m_rx_req     => opb_fifo_flg(6),
+        opb_m_rx_en      => opb_m_rx_en,
+        opb_m_rx_data    => opb_m_rx_data,
+        opb_rx_dma_ctl   => opb_rx_dma_ctl,
+        opb_rx_dma_addr  => opb_rx_dma_addr,
+        opb_rx_dma_num   => opb_rx_dma_num,
+        opb_rx_dma_done  => opb_fifo_flg(14),
+        opb_abort_flg    => opb_abort_flg,
+        opb_m_last_block => opb_m_last_block);
   end generate dma_enable;
 
   dma_disable : if (C_DMA_EN = false) generate
@@ -484,7 +521,8 @@ begin  -- behavior
       underflow         => opb_fifo_flg(5));
 
   fifo_tx_en   <= opb_s_tx_en or opb_m_tx_en;
-  fifo_tx_data <= opb_m_tx_data when (opb_tx_dma_ctl(0) = '1') else
+  fifo_tx_data <= opb_tx_crc_value when (C_CRC_EN and tx_crc_insert = '1') else
+                  opb_m_tx_data when (opb_tx_dma_ctl(0) = '1') else
                   opb_s_tx_data;
 
   --* Receive FIFO
@@ -522,30 +560,30 @@ begin  -- behavior
   rst <= OPB_Rst or opb_ctl_reg(C_OPB_CTL_REG_RST);
 
   opb_fifo_flg(12) <= ss_n;
-  opb_fifo_flg(15)  <= opb_abort_flg;
+  opb_fifo_flg(15) <= opb_abort_flg;
 
 
 
   -- Bit 0 : TX_PROG_EMPTY
-  opb_irq_flg(0) <= opb_fifo_flg(3);
+  opb_irq_flg(0)  <= opb_fifo_flg(3);
   -- Bit 1 : TX_EMPTY
-  opb_irq_flg(1) <= opb_fifo_flg(4);
+  opb_irq_flg(1)  <= opb_fifo_flg(4);
   -- Bit 2 : TX_Underflow
-  opb_irq_flg(2) <= opb_fifo_flg(5);
+  opb_irq_flg(2)  <= opb_fifo_flg(5);
   -- Bit 3 : RX_PROG_FULL
-  opb_irq_flg(3) <= opb_fifo_flg(6);
+  opb_irq_flg(3)  <= opb_fifo_flg(6);
   -- Bit 4 : RX_FULL
-  opb_irq_flg(4) <= opb_fifo_flg(7);
+  opb_irq_flg(4)  <= opb_fifo_flg(7);
   -- Bit 5 : RX_Overflow
-  opb_irq_flg(5) <= opb_fifo_flg(8);
+  opb_irq_flg(5)  <= opb_fifo_flg(8);
   -- Bit 6:  CS_H_TO_L
-  opb_irq_flg(6) <= not opb_fifo_flg(12);
+  opb_irq_flg(6)  <= not opb_fifo_flg(12);
   -- Bit 7:  CS_L_TO_H
-  opb_irq_flg(7) <= opb_fifo_flg(12);
+  opb_irq_flg(7)  <= opb_fifo_flg(12);
   -- Bit 8: TX DMA Done
-  opb_irq_flg(8) <= opb_fifo_flg(13);
+  opb_irq_flg(8)  <= opb_fifo_flg(13);
   -- Bit 9: RX DMA Done
-  opb_irq_flg(9) <= opb_fifo_flg(14);
+  opb_irq_flg(9)  <= opb_fifo_flg(14);
   -- Bit 10: DMA Transfer Abort
   opb_irq_flg(10) <= opb_abort_flg;
 
@@ -567,6 +605,31 @@ begin  -- behavior
   opb_irq <= '1' when (conv_integer(opb_isr) /= 0 and opb_dgie = '1') else
              '0';
 
+
+  -----------------------------------------------------------------------------
+
+  -- clear start_value at power up and soft_reset
+  crc_en  <= opb_ctl_reg(C_OPB_CTL_REG_CRC_EN);
+  crc_clr <= opb_ctl_reg(C_OPB_CTL_REG_CRC_CLR) or rst;
+
+  crc_gen : if (C_CRC_EN) generate
+    crc_core_1 : crc_core
+      generic map (
+        C_SR_WIDTH => C_SR_WIDTH)
+      port map (
+        rst              => rst,
+        opb_clk          => opb_clk,
+        crc_en           => crc_en,
+        crc_clr          => crc_clr,
+        opb_m_last_block => opb_m_last_block,
+        fifo_rx_en       => fifo_rx_en,
+        fifo_rx_data     => fifo_rx_data,
+        opb_rx_crc_value => opb_rx_crc_value,
+        fifo_tx_en       => fifo_tx_en,
+        fifo_tx_data     => fifo_tx_data,
+        tx_crc_insert    => tx_crc_insert,
+        opb_tx_crc_value => opb_tx_crc_value);
+  end generate crc_gen;
 
 
 end behavior;
